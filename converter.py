ba78c3b8a34cab026c5f952c1d6f4e14805591d2
Farzak-12 <fareedzakariyah@gmail.com>
2025-06-08 09:47:43 +0100

    corrected some typos

--- converter.py
+++ converter.py
@@ -1,15 +1,15 @@
-decimal = int(input("Input you decimal number: "))
+decimal = int(input("Input your decimal number: "))
 binary_list = [] #list to store binary digits
 while decimal: 
-    bin = decimal%2 # get the remainder
+    temp_bin = decimal%2 # get the remainder
     decimal = decimal // 2 
-    binary_list.insert(0,bin) #insert at the beginning of the list
-print("The binary equvalent of your number is ",*binary_list, sep="") #print the list as a string without spaces
+    binary_list.insert(0,temp_bin) #insert at the beginning of the list
+print("The binary equivalent of your number is ",*binary_list, sep="") #print the list as a string without spaces
 
 binary = input('Input your binary number: ')
-pow = len(binary) - 1 #calculate the power of 2
+power = len(binary) - 1 #calculate the power of 2
 final_dec = 0 #variable to store the final decimal value
 for number in binary:
-    final_dec += int(number)*(2**pow) #calculate the decimal value
+    final_dec += int(number)*(2**power) #calculate the decimal value
     pow -= 1 #decrease the power of 2
-print("The binary equvalent of your number is ", final_dec)
+print("The decimal equivalent of your number is ", final_dec)
